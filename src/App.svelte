44f539aa2f14b8468c297a219db277cdd33194a5
Marton Horvath <17813215+horvathmarton@users.noreply.github.com>
2021-09-10 13:36:30 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Earth from './Earth.svelte';
+</script>
+
+<main>
+	<Earth />
+</main>
+
+<style>
+	body {
+  margin: 0;
+}
+
+canvas {
+  display: block;
+}
+</style>
---
+++ src/Earth.svelte
@@ -0,0 +1,28 @@
+<script>
+	import * as THREE from "three";
+
+	let renderer = new THREE.WebGLRenderer();
+	let scene = new THREE.Scene();
+	let aspect = window.innerWidth / window.innerHeight;
+	let camera = new THREE.PerspectiveCamera(45, aspect, 0.1, 1500);
+	let cameraRotation = 0;
+	let cameraRotationSpeed = 0.001;
+	let cameraAutoRotation = true;
+	let orbitControls = new THREE.OrbitControls(camera);
+
+	// Lights
+	let spotLight = new THREE.SpotLight(0xffffff, 1, 0, 10, 2);
+
+	// Texture Loader
+	let textureLoader = new THREE.TextureLoader();
+</script>
+
+<main>
+	<div>Earth</div>
+</main>
+
+<style>
+	canvas {
+		display: block;
+	}
+</style>
