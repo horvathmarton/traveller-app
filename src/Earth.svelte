c8cd9e63409281084c27b6b5070259730fc7f427
Marton Horvath <17813215+horvathmarton@users.noreply.github.com>
2021-09-10 14:00:55 +0200

    feat: remove atmosphere

--- src/Earth.svelte
+++ src/Earth.svelte
@@ -8,18 +8,241 @@
 	let cameraRotation = 0;
 	let cameraRotationSpeed = 0.001;
 	let cameraAutoRotation = true;
-	let orbitControls = new THREE.OrbitControls(camera);
 
 	// Lights
 	let spotLight = new THREE.SpotLight(0xffffff, 1, 0, 10, 2);
 
-	// Texture Loader
-	let textureLoader = new THREE.TextureLoader();
+	// Planet Proto
+	let planetProto = {
+		sphere: function (size) {
+			let sphere = new THREE.SphereGeometry(size, 32, 32);
+
+			return sphere;
+		},
+		material: function (options) {
+			let material = new THREE.MeshPhongMaterial();
+			if (options) {
+				for (var property in options) {
+					material[property] = options[property];
+				}
+			}
+
+			return material;
+		},
+		glowMaterial: function (intensity, fade, color) {
+			// Custom glow shader from https://github.com/stemkoski/stemkoski.github.com/tree/master/Three.js
+			let glowMaterial = new THREE.ShaderMaterial({
+				uniforms: {
+					c: {
+						type: "f",
+						value: intensity,
+					},
+					p: {
+						type: "f",
+						value: fade,
+					},
+					glowColor: {
+						type: "c",
+						value: new THREE.Color(color),
+					},
+					viewVector: {
+						type: "v3",
+						value: camera.position,
+					},
+				},
+				vertexShader: `
+        uniform vec3 viewVector;
+        uniform float c;
+        uniform float p;
+        varying float intensity;
+        void main() {
+          vec3 vNormal = normalize( normalMatrix * normal );
+          vec3 vNormel = normalize( normalMatrix * viewVector );
+          intensity = pow( c - dot(vNormal, vNormel), p );
+          gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+        }`,
+				fragmentShader: `
+        uniform vec3 glowColor;
+        varying float intensity;
+        void main() 
+        {
+          vec3 glow = glowColor * intensity;
+          gl_FragColor = vec4( glow, 1.0 );
+        }`,
+				side: THREE.BackSide,
+				blending: THREE.AdditiveBlending,
+				transparent: true,
+			});
+
+			return glowMaterial;
+		},
+		texture: function (material, property, uri) {
+			let textureLoader = new THREE.TextureLoader();
+			textureLoader.crossOrigin = true;
+			textureLoader.load(uri, function (texture) {
+				material[property] = texture;
+				material.needsUpdate = true;
+			});
+		},
+	};
+
+	function createPlanet (options) {
+		// Create the planet's Surface
+		let surfaceGeometry = planetProto.sphere(options.surface.size);
+		let surfaceMaterial = planetProto.material(options.surface.material);
+		let surface = new THREE.Mesh(surfaceGeometry, surfaceMaterial);
+
+		// Nest the planet's Surface and Atmosphere into a planet object
+		let planet = new THREE.Object3D();
+		surface.name = "surface";
+		planet.add(surface);
+
+		// Load the Surface's textures
+		for (let textureProperty in options.surface.textures) {
+			planetProto.texture(
+				surfaceMaterial,
+				textureProperty,
+				options.surface.textures[textureProperty]
+			);
+		}
+
+		return planet;
+	};
+
+	let earth = createPlanet({
+		surface: {
+			size: 0.5,
+			material: {
+				bumpScale: 0.05,
+				specular: new THREE.Color("grey"),
+				shininess: 10,
+			},
+			textures: {
+				map: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/141228/earthmap1k.jpg",
+				bumpMap:
+					"https://s3-us-west-2.amazonaws.com/s.cdpn.io/141228/earthbump1k.jpg",
+				specularMap:
+					"https://s3-us-west-2.amazonaws.com/s.cdpn.io/141228/earthspec1k.jpg",
+			},
+		},
+	});
+
+	// Marker Proto
+	let markerProto = {
+		latLongToVector3: function latLongToVector3(
+			latitude,
+			longitude,
+			radius,
+			height
+		) {
+			var phi = (latitude * Math.PI) / 180;
+			var theta = ((longitude - 180) * Math.PI) / 180;
+
+			var x = -(radius + height) * Math.cos(phi) * Math.cos(theta);
+			var y = (radius + height) * Math.sin(phi);
+			var z = (radius + height) * Math.cos(phi) * Math.sin(theta);
+
+			return new THREE.Vector3(x, y, z);
+		},
+		marker: function marker(size, color, vector3Position) {
+			let markerGeometry = new THREE.SphereGeometry(size);
+			let markerMaterial = new THREE.MeshLambertMaterial({
+				color: color,
+			});
+			let markerMesh = new THREE.Mesh(markerGeometry, markerMaterial);
+			markerMesh.position.copy(vector3Position);
+
+			return markerMesh;
+		},
+	};
+
+	// Place Marker
+	let placeMarker = function (object, options) {
+		let position = markerProto.latLongToVector3(
+			options.latitude,
+			options.longitude,
+			options.radius,
+			options.height
+		);
+		let marker = markerProto.marker(options.size, options.color, position);
+		object.add(marker);
+	};
+
+	// Place Marker At Address
+	function placeMarkerAtAddress(address, color) {
+		let encodedLocation = address.replace(/\s/g, "+");
+		let httpRequest = new XMLHttpRequest();
+
+		httpRequest.open(
+			"GET",
+			"https://maps.googleapis.com/maps/api/geocode/json?address=" +
+				encodedLocation
+		);
+		httpRequest.send(null);
+		httpRequest.onreadystatechange = function () {
+			if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+				let result = JSON.parse(httpRequest.responseText);
+
+				if (result.results.length > 0) {
+					let latitude = result.results[0].geometry.location.lat;
+					let longitude = result.results[0].geometry.location.lng;
+
+					placeMarker(earth.getObjectByName("surface"), {
+						latitude: latitude,
+						longitude: longitude,
+						radius: 0.5,
+						height: 0,
+						size: 0.01,
+						color: color,
+					});
+				}
+			}
+		};
+	};
+
+	// Scene, Camera, Renderer Configuration
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
+
+	camera.position.set(1, 1, 1);
+
+	scene.add(camera);
+	scene.add(spotLight);
+	scene.add(earth);
+
+	// Light Configurations
+	spotLight.position.set(2, 0, 1);
+
+	// Mesh Configurations
+	earth.receiveShadow = true;
+	earth.castShadow = true;
+	earth.getObjectByName("surface").geometry.center();
+
+	// On window resize, adjust camera aspect ratio and renderer size
+	window.addEventListener("resize", function () {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize(window.innerWidth, window.innerHeight);
+	});
+
+	// Main render function
+	function render() {
+		earth.getObjectByName("surface").rotation.y += (1 / 32) * 0.01;
+		if (cameraAutoRotation) {
+			cameraRotation += cameraRotationSpeed;
+			camera.position.y = 0;
+			camera.position.x = 2 * Math.sin(cameraRotation);
+			camera.position.z = 2 * Math.cos(cameraRotation);
+			camera.lookAt(earth.position);
+		}
+		requestAnimationFrame(render);
+		renderer.render(scene, camera);
+	};
+
+	render();
 </script>
 
-<main>
-	<div>Earth</div>
-</main>
+<main></main>
 
 <style>
 	canvas {
